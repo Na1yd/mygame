af27e65867db88baa31907d2e6eab04645f18150
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-18 14:10:42 +1200

    Almost comppleated movement boundries

--- Player/Sprite2D.gd
+++ Player/Sprite2D.gd
@@ -8,11 +8,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("player_up"):
+	if Input.is_action_pressed("player_up") and position.y > -150:
 		position.y -= 10
-	if Input.is_action_pressed("player_down"):
+	if Input.is_action_pressed("player_down") and position.y < 778:
 		position.y += 10
-	if Input.is_action_pressed("player_left"):
+	if Input.is_action_pressed("player_left") and position.x > -280:
 		position.x -= 10
-	if Input.is_action_pressed("player_right"):
+	if Input.is_action_pressed("player_right") and position.x < 1200:
 		position.x += 10
