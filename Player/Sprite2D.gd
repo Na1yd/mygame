7f33c405a65e6a5e5606514da9ef6afbfad850f3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-01 14:13:49 +1200

    Maked bullet

--- Player/Sprite2D.gd
+++ Player/Sprite2D.gd
@@ -2,13 +2,8 @@ extends Sprite2D
 
 var movement_speed = 500
 var bullet_speed = 2000
-var bullet = preload
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
+var bullet = preload("res://Weapons/bullet.tscnbullet.tscn"):
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("player_up") and position.y > -150:
@@ -19,9 +14,12 @@ func _process(delta):
 		position.x -= 10
 	if Input.is_action_pressed("player_right") and position.x < 1551:
 		position.x += 10
-		
-
+	if Input.is_action_just_pressed("LMB"):
+		fire()
+	
 func fire():
 	var bullet_instance = bullet.instance
 	bullet_instance.position = get_global_position()
 	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply-impulsive(vector2(),vector2(bullet_speed,0).rotated(rotation))
+	get_tree() .get_root() .call_diferrend("add_child",bullet_instance)
