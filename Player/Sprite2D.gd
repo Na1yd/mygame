2a77caace32323601550e013e6feb502e81e8330
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-22 14:40:17 +1200

    Added more functions to the gun and started making ways to aim

--- Player/Sprite2D.gd
+++ Player/Sprite2D.gd
@@ -1,8 +1,7 @@
 extends Sprite2D
 
-var movement_speed = 500
-var bullet_speed = 2000
-var bullet = preload("res://Weapons/bullet.tscn")
+@onready var anim = $AnimatedSprite2D
+@onready var bullet_prefab = preload("res://Weapons/bullet.tscn")
 
 func _ready():
 	pass # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +14,7 @@ func _process(delta):
 		position.x -= 10
 	if Input.is_action_pressed("player_right") and position.x < 1551:
 		position.x += 10
+	if Input.is_action_just_pressed("LMB"):
+		var bullet = bullet_prefab.instantiate()
+		bullet.position = position
+		get_parent().add_child(bullet)
