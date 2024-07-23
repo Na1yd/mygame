0bb80a00c9e2c7015db5cf1c674a27c16980b77f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-24 10:23:46 +1200

    Tryed to make bullet directions not working so far

--- Player/Sprite2D.gd
+++ Player/Sprite2D.gd
@@ -1,10 +1,10 @@
 extends Sprite2D
 
 @onready var anim = $AnimatedSprite2D
-@onready var bullet_prefab0 = preload("res://Weapons/bullet_left.tscn")
-@onready var bullet_prefab1 = preload("res://Weapons/bullet_right.tscn")
-@onready var bullet_prefab2 = preload("res://Weapons/bullet_up.tscn")
-@onready var bullet_prefab3 = preload("res://Weapons/bullet_down.tscn")
+@onready var bullet_instant = preload("res://Weapons/bullet.tscn")
+
+
+var bullet_speed = 2000
 func _ready():
 	pass # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,22 +24,13 @@ func _process(delta):
 		position.x += 10
 	if Input.is_action_pressed("player_right"):
 			anim.play("right")
+	look_at(get_global_mouse_position())
+
 	if Input.is_action_just_pressed("LMB"):
-		var bullet = bullet_prefab0.instantiate()
-		bullet.position = position
-		get_parent().add_child(bullet)
-		
-	if Input.is_action_just_pressed("LMB") and anim.play("right") == true:
-		var bullet = bullet_prefab1.instantiate()
-		bullet.position = position
-		get_parent().add_child(bullet)
-		
-	if Input.is_action_just_pressed("LMB") and anim.play("up") == true:
-		var bullet = bullet_prefab2.instantiate()
-		bullet.position = position
-		get_parent().add_child(bullet)
-		
-	if Input.is_action_just_pressed("LMB") and anim.play("down") == true:
-		var bullet = bullet_prefab3.instantiate()
-		bullet.position = position
-		get_parent().add_child(bullet)
+		fire()
+func fire():
+	var bullet = bullet_instant.instantiate()
+	bullet.position = get_global_position()
+	bullet_instant.rotation_degrees = rotation_degrees
+	bullet_instant.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_instant)
