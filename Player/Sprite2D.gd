1c9a6fb5bb6c625d0e7272ad92001e68a743848e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-26 09:22:35 +1200

    Tryed another way to get gun to work. It did not

--- Player/Sprite2D.gd
+++ Player/Sprite2D.gd
@@ -1,13 +1,13 @@
 extends Sprite2D
 
 @onready var anim = $AnimatedSprite2D
-@onready var bullet_instant = preload("res://Weapons/bullet.tscn")
+@export var Bullet : PackedScene
 
 
 var bullet_speed = 2000
 func _ready():
 	pass # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("player_up") and position.y > -150:
 		position.y -= 10
 	if Input.is_action_pressed("player_up"):
@@ -24,13 +24,12 @@ func _process(delta):
 		position.x += 10
 	if Input.is_action_pressed("player_right"):
 			anim.play("right")
-	look_at(get_global_mouse_position())
-
+	look_at()
 	if Input.is_action_just_pressed("LMB"):
-		fire()
-func fire():
-	var bullet = bullet_instant.instantiate()
-	bullet.position = get_global_position()
-	bullet_instant.rotation_degrees = rotation_degrees
-	bullet_instant.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
-	get_tree().get_root().call_deferred("add_child",bullet_instant)
+		shoot()
+
+
+func shoot():
+	var b = Bullet.instantiate()
+	add_child(b)
+	b.transform = $Muzzle.transform
