9ad0d119ded7a09407a80976efda8cd4ce15dec9
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-06 12:44:13 +1200

    Added bourders and meat chunk(bad guy) Also made gun go on a timer so it more like a projectile now

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,36 +1,45 @@
 extends CharacterBody2D
 
+var speed = 5
+
 @onready var anim = $AnimatedSprite2D
 @onready var bullets_L = preload("res://Weapons/bullet_right.tscn")
 @onready var bullets_R = preload("res://Weapons/bullet_left.tscn")
 @onready var bullets_U = preload("res://Weapons/bullet_up.tscn")
 @onready var bullets_D = preload("res://Weapons/bullet_down.tscn")
-@onready var shoot = preload("res://Player/Timer.gd")
+#projectile timer
+func _on_start():
+	$Timer.start()
+
+
+
+
 
 func _ready():
 	pass # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta):
-	if Input.is_action_pressed("player_up") and position.y > -150:
-		position.y -= 10
+	if Input.is_action_pressed("player_up") and position.y > 0:
+		position.y -= speed
 	if Input.is_action_pressed("player_up"):
 		$Sprite2D/AnimatedSprite2D.play("up")
-	if Input.is_action_pressed("player_down") and position.y < 778:
-		position.y += 10
+	if Input.is_action_pressed("player_down") and position.y < 1014:
+		position.y += speed
 	if Input.is_action_pressed("player_down"):
 		$Sprite2D/AnimatedSprite2D.play("down")
-	if Input.is_action_pressed("player_left") and position.x > -280:
-		position.x -= 10
+	if Input.is_action_pressed("player_left") and position.x > 0:
+		position.x -= speed
 	if Input.is_action_pressed("player_left"):
 			$Sprite2D/AnimatedSprite2D.play("left")
-	if Input.is_action_pressed("player_right") and position.x < 1551:
-		position.x += 10
+	if Input.is_action_pressed("player_right") and position.x < 1910:
+		position.x += speed
 	if Input.is_action_pressed("player_right"):
 			$Sprite2D/AnimatedSprite2D.play("right")
 			
 
 
 
+#projectile code
 func _on_timer_timeout():
 	var bullet_L = bullets_L.instantiate()
 	bullet_L.position = position
@@ -48,3 +57,4 @@ func _on_timer_timeout():
 	bullet_D.position = position
 	get_parent().add_child(bullet_D)
 	
+	$Timer.start()
--- Player/Timer.gd
+++ Player/Timer.gd
@@ -1,13 +1,4 @@
 extends Timer
 
 
-func _on_start():
-	$Timer.start()
 
-
-
-func _on_timeout_timeout():
-	$Timer.on_start()
-
-func start():
-	$Timer.start
